bdbe671a99e822bdc263ada8fa1c88e7bbb3de6d
slimerence <39113678+slimerence@users.noreply.github.com>
2018-07-09 09:59:26 +1000

    nav

--- theone/layouts/frontend/_nav.blade.php
+++ theone/layouts/frontend/_nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">
-            TORX
+        <a class="navbar-brand js-scroll-trigger" href="{{ url('/') }}">
+           TORX
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fa fa-bars"></i>
