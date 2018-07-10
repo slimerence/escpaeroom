43e09d40c0775c491c319bd1ce2c0361649bc878
wangyue <18510209803@163.com>
2018-07-11 01:41:06 +1000

    Create basic structure of reservation

---
+++ app/Models/Reservation.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: justinwang
+ * Date: 10/7/18
+ * Time: 3:32 PM
+ */
+
+namespace Smartbro\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Catalog\Product;
+use App\User;
+
+class Reservation extends Model
+{
+    use SoftDeletes;
+
+    const STATUS_PENDING_PAYMENT    = 1;
+    const STATUS_BOOKED             = 2;
+    const STATUS_COMPLETED          = 3;
+    const STATUS_CANCELLED          = 4;
+
+    protected $fillable = [
+        'uuid',
+        'product_id',
+        'user_id',
+        'at_date',
+        'at_time',
+        'at',
+        'participants',
+        'status',
+        'booking_fee_required', // 是否该预定需要预付定金
+        'transaction_number',   // 如果需要预付费, 这里保存付费的流水单号
+        'coupon',               // 如果玩家使用了优惠劵, 填写在这里
+        'notes',
+        'why_cancelled',        // 如果取消, 这里填写原因
+        'score',                // 玩家完成的用时
+        'rating',               // 玩家的评价
+        'customer_comment',     // 玩家消费之后的留言
+        'customer_notified',
+        'shop_owner_notified',
+        'extra',                // 为未来预留的字段
+    ];
+
+    public $dates = [
+        'created_at','updated_at','at'
+    ];
+
+    protected $casts = [
+        'booking_fee_required'=>'boolean',
+        'customer_notified'=>'boolean',
+        'shop_owner_notified'=>'boolean',
+        'extra'=>'array'
+    ];
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    public function customer(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/migrations/2018_07_10_053108_reservation.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Smartbro\Models\Reservation as ReservationModel;
+
+class Reservation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table){
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('user_id');
+
+            // 不使用 Time slot 了, 因为时间关系
+            $table->date('at_date');
+            $table->time('at_time');
+            $table->dateTime('at')->nullable();
+            $table->unsignedSmallInteger('participants');   // 参加人数
+
+            $table->unsignedSmallInteger('status')->default(ReservationModel::STATUS_BOOKED);
+
+            $table->boolean('booking_fee_required')->default(false); // 是否该预定需要预付定金
+            $table->string('transaction_number')->nullable(); // 如果需要预付费, 这里保存付费的流水单号
+
+            $table->string('coupon',20)->nullable();    // 如果玩家使用了优惠劵, 填写在这里
+            $table->text('notes')->nullable();
+            $table->text('why_cancelled')->nullable();  // 如果取消, 这里填写原因
+
+            // 玩家的完成用时
+            $table->unsignedInteger('score')->default(0);   // 玩家完成的用时
+            $table->unsignedInteger('rating')->default(5);  // 玩家的评价
+            $table->text('customer_comment')->nullable();   // 玩家消费之后的留言
+
+            // 是否提醒的功能字段
+            $table->boolean('customer_notified')->default(false);
+            $table->boolean('shop_owner_notified')->default(false);
+
+            // 为未来预留的字段
+            $table->json('extra')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
